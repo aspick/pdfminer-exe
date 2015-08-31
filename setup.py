60aa775ddc532232c8abf81b45bb036aae62e3d5
Yugo Terada <yugo@18th-lab.com>
2015-08-31 23:32:11 +0900

    add more options to py2exe

--- setup.py
+++ setup.py
@@ -3,4 +3,8 @@
 from distutils.core import setup
 import py2exe
 
-setup(console=['pdfminer/tools/pdf2txt.py'])
+setup(
+    options = {'py2exe': {'bundle_files':1}},
+    zipfile = None,
+    console = [{'script':'pdfminer/tools/pdf2txt.py'}]
+)
